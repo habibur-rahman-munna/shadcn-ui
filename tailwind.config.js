555b10696c6310420d69f9a84d8a4905714349ce
munna <habiburrahmanmunna56@gmail.com>
2025-02-01 12:33:14 +0600

    update-2

--- tailwind.config.js
+++ tailwind.config.js
@@ -4,6 +4,9 @@ export default {
     content: ["./index.html", "./src/**/*.{ts,tsx,js,jsx}"],
   theme: {
   	extend: {
+		fontFamily:{
+			myFont: '__GeistSans_ac79ff'
+		},
   		borderRadius: {
   			lg: 'var(--radius)',
   			md: 'calc(var(--radius) - 2px)',
